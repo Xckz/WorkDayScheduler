26412535b1f931d67f3eb1270a3dd9775f706d66
Lyman Connor <eckzly@gmail.com>
2021-09-07 15:14:04 -0400

    added icon and typefields to appropriate boxes

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,10 +1,16 @@
 // variable reference guide
 
 var timeDisplayEl = $("#time-display");
+var descriptionEl = $(".description");
 
+// handles displaying time at the top of the page
 function displayTime() {
     var now = moment().format("MMM Do, YYYY");
     $("#time-display").text(now);
 }
 
 setInterval(displayTime, 1000);
+
+// planEl.text("text");
+
+$("description").click();

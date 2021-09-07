117c045dd96a29cd77ea0714ac7b46b7801c35eb
Lyman Connor <eckzly@gmail.com>
2021-09-07 16:41:35 -0400

    completed css change based on time

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,7 +1,9 @@
 // variable reference guide
 
-var timeDisplayEl = $("#time-display");
+var currentTime = moment().format("H");
 var descriptionEl = $(".description");
+var timeDisplayEl = $("#time-display");
+var timeOfDay = $(".log");
 
 // handles displaying time at the top of the page
 function displayTime() {
@@ -9,8 +11,24 @@ function displayTime() {
     $("#time-display").text(now);
 }
 
+// handles changing the schedule event box based on the time of day
+var scheduleTime = () => {
+    for (var i = 0; i < timeOfDay.length; i++) {
+        var timeOfDayEl = timeOfDay[i].id;
+
+        var timeChange = document.getElementById(timeOfDay[i].id);
+
+        if (timeOfDayEl < currentTime) {
+            $(timeChange).addClass("past");
+        } else if (timeOfDayEl > currentTime) {
+            $(timeChange).addClass("future");
+        } else {
+            $(timeChange).addClass("present");
+        }
+    }
+};
+
+setInterval(scheduleTime, 1000);
 setInterval(displayTime, 1000);
 
-// planEl.text("text");
-
-$("description").click();
+console.log(currentTime);
